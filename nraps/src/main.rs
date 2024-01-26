d7757ffa977fb627f48a9da302221e8b9c339031
jurskidl <jurskidylan@yahoo.com>
2024-01-26 15:07:08 -0500

    Can read a text file
    Can now read text file. Saves none of the info though

---
+++ nraps/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+fn main() {
+    let file_path = "../SampleInputFile.txt";
+    // --snip--
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
