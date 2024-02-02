e05d7533cf037536ceea9fc376cfdd47a337f1ed
jurskidl <jurskidylan@yahoo.com>
2024-02-02 18:51:08 -0500

    finished the code

--- nraps/src/main.rs
+++ nraps/src/main.rs
@@ -185,8 +185,4 @@ fn main() {
     process_input();
 
     println!("In file {}", file_path);
-
-    //let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    //println!("With text:\n{contents}");
 }
---
+++ nraps/target/debug/build/proc-macro2-0bee600ef3f34170/out/proc_macro2.d
@@ -0,0 +1,7 @@
+C:\Users\jursk\OneDrive\Documents\GitHub\SCA_code_python\NRAPS\nraps\target\debug\build\proc-macro2-0bee600ef3f34170\out\libproc_macro2.rmeta: build\probe.rs
+
+C:\Users\jursk\OneDrive\Documents\GitHub\SCA_code_python\NRAPS\nraps\target\debug\build\proc-macro2-0bee600ef3f34170\out\proc_macro2.d: build\probe.rs
+
+build\probe.rs:
+
+# env-dep:RUSTC_BOOTSTRAP
