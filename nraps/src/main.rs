b806fb3583609dc16b997b6afd32f5e30642bbac
jurskidl <jurskidylan@yahoo.com>
2024-02-02 09:12:44 -0500

    Mostly Complete now

--- nraps/src/main.rs
+++ nraps/src/main.rs
@@ -44,11 +44,48 @@ fn process_input() {
         .filter(|x| !x.starts_with("#") && x.contains("="))
         .collect();
 
-    let (var_names, _var_values): (Vec<&str>, Vec<&str>) =
+    let (var_names, var_values): (Vec<&str>, Vec<&str>) =
         lines.iter().map(|x| x.split_once("=").unwrap()).unzip();
 
     let var_names: Vec<String> = var_names.into_iter().map(|x| x.to_string()).collect();
 
+    let positions = positions(var_names);
+
+    let variables = Variables {
+        solution: var_values[positions[0]].trim().parse().unwrap(),
+        testcase: var_values[positions[1]].trim().parse().unwrap(),
+        analk: var_values[positions[2]].trim().parse().unwrap(),
+        energygroups: var_values[positions[3]].trim().parse().unwrap(),
+        solver: match var_values[positions[4]].trim() {
+            "1" => Solver::Gaussian,
+            "2" => Solver::Jacobian,
+            "3" => Solver::Sor,
+            _ => Solver::LinAlg,
+        },
+        generations: var_values[positions[5]].trim().parse().unwrap(),
+        histories: var_values[positions[6]].trim().parse().unwrap(),
+        skip: var_values[positions[7]].trim().parse().unwrap(),
+        numass: var_values[positions[8]].trim().parse().unwrap(),
+        numrods: var_values[positions[9]].trim().parse().unwrap(),
+        roddia: var_values[positions[10]].trim().parse().unwrap(),
+        rodpitch: var_values[positions[11]].trim().parse().unwrap(),
+        mpfr: var_values[positions[12]].trim().parse().unwrap(),
+        mpwr: var_values[positions[13]].trim().parse().unwrap(),
+        boundl: var_values[positions[14]].trim().parse().unwrap(),
+        boundr: var_values[positions[15]].trim().parse().unwrap(),
+    };
+
+    // let indices = var_names
+    //     .into_iter()
+    //     .position(|x| x.contains(variable_names.iter().map(|x| x)).unwrap())
+    //     .collect();
+
+    for index in 0..lines.len() {
+        println!("{}", lines[index]);
+    }
+}
+
+fn positions(vector: Vec<String>) -> Vec<usize> {
     let variable_names: [&str; 23] = [
         "solution",
         "testcase",
@@ -75,14 +112,19 @@ fn process_input() {
         "chit",
     ];
 
-    // let indices = var_names
-    //     .into_iter()
-    //     .position(|x| x.contains(variable_names.iter().map(|x| x)).unwrap())
-    //     .collect();
+    let mut positions: Vec<usize> = vec![];
 
-    // for index in 0.._var_names.len() {
-    //     println!("{}", _var_names[index]);
-    // }
+    for index in 0..variable_names.len() {
+        positions.push(
+            vector
+                .iter()
+                .position(|x| x.trim() == variable_names[index])
+                .unwrap(),
+        );
+        println!("{}", positions[index]);
+    }
+
+    positions
 }
 
 fn main() {
