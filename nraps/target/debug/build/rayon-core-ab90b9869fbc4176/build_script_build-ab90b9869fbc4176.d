4a6ad68e14ee7318dbfa7ab19d56a5c312e5dc83
jurskidl <jurskidylan@yahoo.com>
2024-02-07 10:37:41 -0500

    IDK

--- nraps/target/debug/build/rayon-core-ab90b9869fbc4176/build_script_build-ab90b9869fbc4176.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jursk\OneDrive\Documents\GitHub\SCA_code_python\NRAPS\nraps\target\debug\build\rayon-core-ab90b9869fbc4176\build_script_build-ab90b9869fbc4176.exe: C:\Users\jursk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rayon-core-1.12.1\build.rs
-
-C:\Users\jursk\OneDrive\Documents\GitHub\SCA_code_python\NRAPS\nraps\target\debug\build\rayon-core-ab90b9869fbc4176\build_script_build-ab90b9869fbc4176.d: C:\Users\jursk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rayon-core-1.12.1\build.rs
-
-C:\Users\jursk\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rayon-core-1.12.1\build.rs:
--- nraps/target/debug/deps/nraps-792bb831f7819eae.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jursk\OneDrive\Documents\GitHub\SCA_code_python\NRAPS\nraps\target\debug\deps\libnraps-792bb831f7819eae.rmeta: src\main.rs
-
-C:\Users\jursk\OneDrive\Documents\GitHub\SCA_code_python\NRAPS\nraps\target\debug\deps\nraps-792bb831f7819eae.d: src\main.rs
-
-src\main.rs:
