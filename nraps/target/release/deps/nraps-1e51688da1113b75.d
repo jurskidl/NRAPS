8bd3f77533052e677d4ee130c8412a6c8b99b6b8
jurskidl <jurskidylan@yahoo.com>
2024-02-07 18:50:30 -0500

    I think I am done testing this

--- nraps/target/release/deps/nraps-1e51688da1113b75.d
+++
@@ -1,5 +0,0 @@
-/home/dylan/GitHub/NRAPS/nraps/target/release/deps/nraps-1e51688da1113b75: src/main.rs
-
-/home/dylan/GitHub/NRAPS/nraps/target/release/deps/nraps-1e51688da1113b75.d: src/main.rs
-
-src/main.rs:
--- nraps/target/release/nraps.d
+++
@@ -1,1 +0,0 @@
-/home/dylan/GitHub/NRAPS/nraps/target/release/nraps: /home/dylan/GitHub/NRAPS/nraps/src/main.rs
