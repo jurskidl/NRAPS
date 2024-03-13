3974b3ce50c5cad8016a79980e8944a660a1e9d7
jurskidl <jurskidylan@yahoo.com>
2024-03-13 14:48:42 -0400

    It is plotting now

---
+++ nraps/target/debug/deps/nraps-aff40901e604c8b4.d
@@ -0,0 +1,5 @@
+C:\Users\jursk\OneDrive\Documents\GitHub\SCA_code_python\NRAPS\nraps\target\debug\deps\libnraps-aff40901e604c8b4.rmeta: src\main.rs
+
+C:\Users\jursk\OneDrive\Documents\GitHub\SCA_code_python\NRAPS\nraps\target\debug\deps\nraps-aff40901e604c8b4.d: src\main.rs
+
+src\main.rs:
--- plot.py
+++ plot.py
@@ -1,22 +1,39 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import csv
+import numpy as np
 
 y1 = y2 = y3 = y4 = []
 
 count = 1
 for line in open('k_eff.csv', 'r'):
-    k_eff = line
+    k_eff = np.array(line.strip().split(',')).astype(float)
+
 for line in open('interface.csv', 'r'):
     if count == 1:
-        flux0 = line.strip().split(',')
+        flux0 = np.array(line.strip().split(',')).astype(float)
     elif count == 2:
-        flux1 = line.strip().split(',')
+        flux1 = np.array(line.strip().split(',')).astype(float)
     else:
-        fission = line.strip().split(',')
+        fission = np.array(line.strip().split(',')).astype(float)
 
     count += 1
 
-for item in k_eff:
-    float(item)
+plt.plot(k_eff, label='multplication factor')
+plt.title('Multiplication Factor')
+# plt.show()
+plt.savefig('./k_eff.svg', bbox_inches='tight')
 
-print(k_eff)
+plt.plot(flux0, label='fast flux')
+plt.title('Fast Flux')
+# plt.show()
+plt.savefig('./fast_flux.svg', bbox_inches='tight')
+
+plt.plot(flux1, label='thermal flux')
+plt.title('Thermal Flux')
+# plt.show()
+plt.savefig('./thermal_flux.svg', bbox_inches='tight')
+
+plt.plot(fission, label='fission')
+plt.title('Fission Density')
+# plt.show()
+plt.savefig('./fission.svg', bbox_inches='tight')
